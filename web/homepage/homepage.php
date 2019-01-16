92bcc112eb0a9286ce822a1cfc218f7ee4b83c84
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-01-16 09:37:16 -0700

    homepage folder and php footer and header

---
+++ web/homepage/homepage.php
@@ -0,0 +1,18 @@
+  <div class="container">
+  	<nav class="row"></nav>
+    <div class="row" >
+    	<div class="col-lg-12 col-med-12 col-sm-12 col-xs-12"></div>
+    </div>
+  </div>
+	  
+	  
+	<!-- Toolbar for buttons and input-->
+	<div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+	  <input type="text" class="form-control" id="div1Color" name="div1Color">
+
+	  <button class="btn btn-info" id="btn1" type="button" onclick="changeDiv1Color()">Change Color</button>
+	  <button class="btn btn-info" id="btn2" type="button" onclick="clicked()">Click Me</button>
+	  <button class="btn btn-info" id="btn3" type="button" onclick="toggleHideDiv3()">Toggle Hide Div 3</button>
+
+	  </div>
+	  
