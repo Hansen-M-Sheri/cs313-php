8f1b6fc332057392070f6c8dd4d0ba0b6a78e254
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-01-16 13:11:09 -0700

    cards changed to vertical

--- web/homepage/homepage.php
+++ web/homepage/homepage.php
@@ -1,40 +1,48 @@
 <?php include "templates/header.php"; ?>
 
-<div class="container">
-  	  <!-- Nav bar-->
-  	  <nav class="navbar navbar-expand-lg navbar-light ">
-  	    <a class="navbar-brand" hreg="#">SH</a>
-  	  </nav>
-  	  <!-- Jumbotron -->
-  	  <div class="jumbotron bg-secondary" id="banner">
-  	    <h1>Sheri Hansen</h1>
-  	    <p id="quote">The meaning of life is to find your gift. The purpose of life is to give it away. <br/> 
-  	    Anonymous</p>
-  		  <button type="button" class="btn-info">Assignments</button>
-    	</div>
-    	<!-- Cards -->
-      <div class="row mb-2" id="interests">
-        <div class="col-md-6  col-xs-12 " id="interest1">
-          <div class="card">
-            <div class="card-body ">
-              <h2>Family</h2>
-              <p>I have an amazing husband and 5 lovely children. We love to laugh together, and simply enjoy being together. </p>
-            </div>
-          </div>
-          <div class="col-md-6 col-xs-12" id="interest2">
-          <div class="card" >
-            <div class="card-body">
-              <h2>Succulents</h2>
-              <p>I received my first succulent as a gift last year, and am in love! Despite the lack of light and cold weather here, I have managed to keep it alive so far. </p>
-            </div>
-          </div>
-          <div class="col-md-6 col-xs-12" id="interest3">
-          <div class="card ">
-            <div class="card-body">
-              <h2>School</h2>
-              <p>I am a senior at BYUI and am excited and nervous to only have 4 semesters left till I graduate</p>
-            </div>
-          </div>
-        </div>
+ <div class="container">
+    <!-- Nav bar-->
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <a class="navbar-brand" hreg="#">SH</a>
+    </nav>
+    <!-- Jumbotron -->
+    <div class="jumbotron bg-secondary" id="banner">
+      <h1>Sheri Hansen</h1>
+      <p id="quote">The meaning of life is to find your gift. The purpose of life is to give it away.
+        <br/> Anonymous
+      </p>
+      <button type="button" class="btn-info">Assignments</button>
+    </div>
+    <!-- Cards -->
+    <div class="row">
+      <div class="col-md-4" id="interest1">
+        <img class="card-img-top" data-src="../images/typingOnLaptop.jpeg">
+        <div class="card  mb-4 box-shadow">
+          <div class="card-body ">
+            <h2 class="card-title">Family</h2>
+            <p class="card-text">I have an amazing husband and 5 lovely children. We love to laugh together, and simply enjoy being together. </p>
+          </div><!--body-->
+        </div><!--card-->
+      </div><!--column-->
+      <div class="col-md-4" id="interest1">
+        <img class="card-img-top" data-src="../images/typingOnLaptop.jpeg">
+        <div class="card  mb-4 box-shadow">
+          <div class="card-body ">
+            <h2 class="card-title">School</h2>
+            <p class="card-text">I am a senior at BYUI and am excited and nervous to only have 4 semesters left till I graduate</p>
+         </div><!--body-->
+        </div><!--card-->
+      </div><!--column-->
+      <div class="col-md-4" id="interest1">
+        <img class="card-img-top" data-src="../images/typingOnLaptop.jpeg">
+        <div class="card  mb-4 box-shadow">
+          <div class="card-body ">
+            <h2 class="card-title">Succulents</h2>
+            <p class="card-text">I received my first succulent as a gift last year, and am in love! Despite the lack of light and cold weather here, I have managed to keep it alive so far. </p>
+          </div><!--body-->
+        </div><!--card-->
+      </div><!--column-->
+  </div><!--row-->
+</div><!--container-->
 
 <?php include "templates/footer.php"; ?>
