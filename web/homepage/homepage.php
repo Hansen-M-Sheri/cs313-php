4dead889ce95acafde2d58f78afd013379504733
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-01-18 12:54:25 -0700

    add div for overlay text on img

--- web/homepage/homepage.php
+++ web/homepage/homepage.php
@@ -19,30 +19,40 @@
         <div class="topImg">
           <img class="card-img-top" src="images/books.jpeg">
           <div class="overlay">
-              <p class="text">Family</p>
+              <p class="text">Reading</p>
             </div>
         </div>
         <div class="card  mb-4 box-shadow">
           <div class="card-body ">
-            <h2 class="card-title">Reading</h2>
+            <!--<h2 class="card-title">Reading</h2>-->
             <p class="card-text">I love to read and can't wait till I have a little more time to read all of the books I haven't gotten to yet </p>
           </div><!--body-->
         </div><!--card-->
       </div><!--column-->
       <div class="col-md-4" id="interest1">
-        <img class="card-img-top" src="images/typingOnLaptop.jpeg">
+        <div class="topImg">
+          <img class="card-img-top" src="images/typingOnLaptop.jpeg">
+          <div class="overlay">
+                <p class="text">School</p>
+          </div>
+        </div>
         <div class="card  mb-4 box-shadow">
           <div class="card-body ">
-            <h2 class="card-title">School</h2>
+            <!--<h2 class="card-title">School</h2>-->
             <p class="card-text">I am a senior at BYUI and am excited and nervous to only have 4 semesters left till I graduate</p>
          </div><!--body-->
         </div><!--card-->
       </div><!--column-->
       <div class="col-md-4" id="interest1">
-        <img class="card-img-top" src="images/succulent.jpeg">
+        <div class="topImg">
+          <img class="card-img-top" src="images/succulent.jpeg">
+          <div class="overlay">
+            <p class="text">Succulents</p>
+          </div>
+        </div>
         <div class="card  mb-4 box-shadow">
           <div class="card-body ">
-            <h2 class="card-title">Succulents</h2>
+            <!--<h2 class="card-title">Succulents</h2>-->
             <p class="card-text">I received my first succulent as a gift last year, and am in love! Despite the lack of light and cold weather here, I have managed to keep it alive so far. </p>
           </div><!--body-->
         </div><!--card-->
