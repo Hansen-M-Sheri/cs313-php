c5129a6ebf7bdd4b7bb9851f503303d0d97a7843
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-02 10:23:16 -0700

    add db insert

--- web/nvelopes/includes/login_inc.php
+++ web/nvelopes/includes/login_inc.php
@@ -4,12 +4,88 @@
 if(isset($_POST['submitLogin'])){
 	include_once 'dbh.php';
 
-	$email = $_POST['email'];
-	echo $email;
-}
-elseif (isset($_POST['submitSignup'])){
+	$email = htmlspecialchars($_POST['email']);
+	$pwd = htmlspecialchars($_POST['pwd']);
+
+	//Error handling
+	//Check for empty fields
+	if(empty($email)) || (empty($pwd)){
+		header("Location: ../login.php?login=empty");
+	}
+	else{
+		//check if input characters are valid
+		//if(!preg_match("/^[a-zA-Z]*$", $firstName))
+		//check if email is valid
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+			header("Location: ../login.php?login=email");
+		}
+		else{
+			//verify email exists in db
+			$stmt = $db->prepare("SELECT email FROM users WHERE EXISTS users.email = '$email'");
+			$stmt->execute();
+			$rows = $stmt->fetchALL(PDO::FETCH_ASSOC);
+			// foreach ($rows as $key => $value) {
+			// 	# code...
+			// }
+		}
+	}
 
 }
+elseif (isset($_POST['submitSignup'])){
+	include_once 'dbh.php';
+
+	$fName = htmlspecialchars($_POST['fName']);
+	$lName = htmlspecialchars($_POST['lName']);
+	$email = htmlspecialchars($_POST['email']);
+	$pwd   = htmlspecialchars($_POST['pwd']);
+	// $secret1 = htmlspecialchars($_POST['pwd']);
+	// $secret2 = htmlspecialchars($_POST['pwd']);
+	$secret1 = "secret 1";
+	$secret2 = "secret 2";
+
+	//Error handling
+	//Check for empty fields
+	if(empty($fName)) || (empty($lName) || empty($email)) || (empty($pwd) || empty($secret1)) || (empty($secret2)){
+		header("Location: ../login.php?login=empty");
+		exit();
+	}
+	else{
+		//check if input characters are valid
+		if(!preg_match("/^[a-zA-Z]*$", $fName) || !preg_match("/^[a-zA-Z]*$", $lName)){
+			header("Location: ../login.php?login=invalidName");
+			exit();
+		}
+		else{
+			//check if email is valid 
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				header("Location: ../login.php?login=email");
+				exit();
+			}
+			else {
+				//verify email exists in db
+				$stmt = $db->prepare("SELECT * FROM users WHERE users.email = '$email'");
+				$stmt->execute();
+				$rows = $stmt->fetchALL(PDO::FETCH_ASSOC);
+				if ($rows > 0 ){
+					header("Location: ../login.php?signup=userTaken");
+					exit();
+				} 
+				else {
+					$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+					//insert user into database
+					$sql = "INSERT INTO users (fName, lName, email, pwd, secret1, secret2) VALUES ('$fName', '$lName', '$email', '$hashedPwd', '$secret1', '$secret2');";
+					$stmt = $db->prepare($sql);
+					$stmt->execute();
+
+				}
+				// foreach ($rows as $key => $value) {
+				// 	# code...
+				// }
+				echo var_dump($rows);
+
+			}
+		}
+		
 else{
 	header("Location: ../login.php");
 }
