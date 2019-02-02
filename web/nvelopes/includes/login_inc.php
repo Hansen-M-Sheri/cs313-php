2f6ac66b3de2034b04f9529a7fdfdf845fe8a960
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-02 11:58:37 -0700

    locating 500

--- web/nvelopes/includes/login_inc.php
+++ web/nvelopes/includes/login_inc.php
@@ -53,49 +53,49 @@ elseif (isset($_POST['submitSignup'])){
 		exit();
 	}
 	else{
-		//check if input characters are valid
-		if(!preg_match("/^[a-zA-Z]*$", $fName) || !preg_match("/^[a-zA-Z]*$", $lName)){
-			header("Location: ../login.php?login=invalidName");
-			exit();
-		}
-		else{
-			//check if email is valid 
-			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-				header("Location: ../login.php?login=email");
-				exit();
-			}
-			else {
-				//verify email exists in db
-				$stmt = $db->prepare("SELECT * FROM users WHERE users.email = '$email'");
-				$stmt->execute();
-				$rows = $stmt->fetchALL(PDO::FETCH_ASSOC);
-				if ($rows > 0 ){
-					header("Location: ../login.php?signup=userTaken");
-					exit();
-				} 
-				else {
-					$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-					//insert user into database
-					$sql = 'INSERT INTO users (fName, lName, email, pwd, secret1, secret2) VALUES (:fName, :lName, :email, :hashedPwd, :secret1, :secret2)';
-					$stmt = $db->prepare($sql);
+		// //check if input characters are valid
+		// if(!preg_match("/^[a-zA-Z]*$", $fName) || !preg_match("/^[a-zA-Z]*$", $lName)){
+		// 	header("Location: ../login.php?login=invalidName");
+		// 	exit();
+		// }
+		// else{
+		// 	//check if email is valid 
+		// 	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+		// 		header("Location: ../login.php?login=email");
+		// 		exit();
+		// 	}
+		// 	else {
+		// 		//verify email exists in db
+		// 		$stmt = $db->prepare("SELECT * FROM users WHERE users.email = '$email'");
+		// 		$stmt->execute();
+		// 		$rows = $stmt->fetchALL(PDO::FETCH_ASSOC);
+		// 		if ($rows > 0 ){
+		// 			header("Location: ../login.php?signup=userTaken");
+		// 			exit();
+		// 		} 
+		// 		else {
+		// 			$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+		// 			//insert user into database
+		// 			$sql = 'INSERT INTO users (fName, lName, email, pwd, secret1, secret2) VALUES (:fName, :lName, :email, :hashedPwd, :secret1, :secret2)';
+		// 			$stmt = $db->prepare($sql);
 
-					//pass values to statement
-					$stmt->bindValue(':fName', $fName);
-					$stmt->bindValue(':lName', $lName);
-					$stmt->bindValue(':email', $email);
-					$stmt->bindValue(':hashedPwd', $hashedPwd);
-					$stmt->bindValue(':secret1', $secret1);
-					$stmt->bindValue(':secret2', $secret2);
+		// 			//pass values to statement
+		// 			$stmt->bindValue(':fName', $fName);
+		// 			$stmt->bindValue(':lName', $lName);
+		// 			$stmt->bindValue(':email', $email);
+		// 			$stmt->bindValue(':hashedPwd', $hashedPwd);
+		// 			$stmt->bindValue(':secret1', $secret1);
+		// 			$stmt->bindValue(':secret2', $secret2);
 					
-					$stmt->execute();
+		// 			$stmt->execute();
 
-					echo $db->lastInsertID('users_id_seq');
+		// 			echo $db->lastInsertID('users_id_seq');
 
-				}
-				// foreach ($rows as $key => $value) {
-				// 	# code...
-				// }
-				echo var_dump($rows);
+		// 		}
+		// 		// foreach ($rows as $key => $value) {
+		// 		// 	# code...
+		// 		// }
+		// 		echo var_dump($rows);
 
 			}
 		}
