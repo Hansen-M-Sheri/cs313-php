f5689e74226e0ec1d633d6ec194d48aba88c318b
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-02 12:13:39 -0700

    fix extra } removed

--- web/nvelopes/includes/dbh_inc.php
+++ web/nvelopes/includes/dbh_inc.php
@@ -15,13 +15,13 @@
 		  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 		  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		}
-		catch (PDOException $ex)
-		{
-		  echo 'Error!: ' . $ex->getMessage();
-		  die();
-		}
 	}
+	catch (PDOException $ex)
+	{
+	  echo 'Error!: ' . $ex->getMessage();
+	  die();
+	}
+	
 
 	//query db & echo scriptures
 	// $stmt = $db->prepare('SELECT * FROM public.scriptures');
