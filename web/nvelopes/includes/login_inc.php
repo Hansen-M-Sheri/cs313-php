f41495f278dd837ec70c9f7f9d11f51301c349d3
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-02 12:27:41 -0700

    added echo lines for testing

--- web/nvelopes/includes/login_inc.php
+++ web/nvelopes/includes/login_inc.php
@@ -37,67 +37,73 @@ if(isset($_POST['submitLogin'])){
 }
 elseif (isset($_POST['submitSignup'])){
 
-	// $fName = htmlspecialchars($_POST['fName']);
-	// $lName = htmlspecialchars($_POST['lName']);
-	// $email = htmlspecialchars($_POST['email']);
-	// $pwd   = htmlspecialchars($_POST['pwd']);
-	// // $secret1 = htmlspecialchars($_POST['pwd']);
-	// // $secret2 = htmlspecialchars($_POST['pwd']);
-	// $secret1 = "secret 1";
-	// $secret2 = "secret 2";
+	$fName = htmlspecialchars($_POST['fName']);
+	$lName = htmlspecialchars($_POST['lName']);
+	$email = htmlspecialchars($_POST['email']);
+	$pwd   = htmlspecialchars($_POST['pwd']);
+	// $secret1 = htmlspecialchars($_POST['pwd']);
+	// $secret2 = htmlspecialchars($_POST['pwd']);
+	$secret1 = "secret 1";
+	$secret2 = "secret 2";
 
-	// //Error handling
-	// //Check for empty fields
-	// if(empty($fName) || empty($lName) || empty($email) || empty($pwd) || empty($secret1) || empty($secret2)){
-	// 	header("Location: ../login.php?login=empty");
-	// 	exit();
-	// }
-	// else{
-		// //check if input characters are valid
-		// if(!preg_match("/^[a-zA-Z]*$", $fName) || !preg_match("/^[a-zA-Z]*$", $lName)){
-		// 	header("Location: ../login.php?login=invalidName");
-		// 	exit();
-		// }
-		// else{
-		// 	//check if email is valid 
-		// 	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-		// 		header("Location: ../login.php?login=email");
-		// 		exit();
-		// 	}
-		// 	else {
-		// 		//verify email exists in db
-		// 		$stmt = $db->prepare("SELECT * FROM users WHERE users.email = '$email'");
-		// 		$stmt->execute();
-		// 		$rows = $stmt->fetchALL(PDO::FETCH_ASSOC);
-		// 		if ($rows > 0 ){
-		// 			header("Location: ../login.php?signup=userTaken");
-		// 			exit();
-		// 		} 
-		// 		else {
-		// 			$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-		// 			//insert user into database
-		// 			$sql = 'INSERT INTO users (fName, lName, email, pwd, secret1, secret2) VALUES (:fName, :lName, :email, :hashedPwd, :secret1, :secret2)';
-		// 			$stmt = $db->prepare($sql);
+	//Error handling
+	//Check for empty fields
+	if(empty($fName) || empty($lName) || empty($email) || empty($pwd) || empty($secret1) || empty($secret2)){
+		header("Location: ../login.php?login=empty");
+		echo var_dump(__LINE__);
+		exit();
+	}
+	else{
+		//check if input characters are valid
+		if(!preg_match("/^[a-zA-Z]*$", $fName) || !preg_match("/^[a-zA-Z]*$", $lName)){
+			header("Location: ../login.php?login=invalidName");
+			echo var_dump(__LINE__);
+			exit();
+		}
+		else{
+			//check if email is valid 
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				header("Location: ../login.php?login=email");
+				echo var_dump(__LINE__);
+				exit();
+			}
+			else {
+				echo var_dump(__LINE__);
+				//verify email exists in db
+				$stmt = $db->prepare("SELECT * FROM users WHERE users.email = '$email'");
+				$stmt->execute();
+				$rows = $stmt->fetchALL(PDO::FETCH_ASSOC);
+				if ($rows > 0 ){
+					echo var_dump(__LINE__);
+					header("Location: ../login.php?signup=userTaken");
+					exit();
+				} 
+				else {
+					echo var_dump(__LINE__);
+					$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+					//insert user into database
+					$sql = 'INSERT INTO users (fName, lName, email, pwd, secret1, secret2) VALUES (:fName, :lName, :email, :hashedPwd, :secret1, :secret2)';
+					$stmt = $db->prepare($sql);
 
-		// 			//pass values to statement
-		// 			$stmt->bindValue(':fName', $fName);
-		// 			$stmt->bindValue(':lName', $lName);
-		// 			$stmt->bindValue(':email', $email);
-		// 			$stmt->bindValue(':hashedPwd', $hashedPwd);
-		// 			$stmt->bindValue(':secret1', $secret1);
-		// 			$stmt->bindValue(':secret2', $secret2);
+					//pass values to statement
+					$stmt->bindValue(':fName', $fName);
+					$stmt->bindValue(':lName', $lName);
+					$stmt->bindValue(':email', $email);
+					$stmt->bindValue(':hashedPwd', $hashedPwd);
+					$stmt->bindValue(':secret1', $secret1);
+					$stmt->bindValue(':secret2', $secret2);
 					
-		// 			$stmt->execute();
+					$stmt->execute();
 
-		// 			echo $db->lastInsertID('users_id_seq');
+					echo $db->lastInsertID('users_id_seq');
 
-		// 		}
-		// 		// foreach ($rows as $key => $value) {
-		// 		// 	# code...
-		// 		// }
-		// 		echo var_dump($rows);
+				}
+				// foreach ($rows as $key => $value) {
+				// 	# code...
+				// }
+				echo var_dump($rows);
 
-			// }
+			}
 		}
 		
 else{
