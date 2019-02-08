98ae72c51fe2e9915471c147159cc9055abaae6b
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-08 10:57:28 -0700

    echo out php variables

--- web/nvelopes/setup.php
+++ web/nvelopes/setup.php
@@ -64,10 +64,11 @@ else {
 						<div class="row">
 					      <div class="col-md-3 ">
 					        <div class="card-container">
+					        	<h4><?php echo $row['name']?></h4>
 					        	<i class="far fa-envelope fa-8x icon"></i>
 					          <div class="card-body">
-					            <h4><?php $row['name']?></h4>
-					            <h4><?php $row['total']?></h4>
+					            
+					            <h4><?php echo $row['total']?></h4>
 					          </div><!--body-->
 					        </div><!--card-->
 				<?php	}
