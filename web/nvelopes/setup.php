2902230f2f747daf43623b93fd8ceffd015b7aaa
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-08 14:26:17 -0700

    adding links for stylesheet

--- web/nvelopes/setup.php
+++ web/nvelopes/setup.php
@@ -40,6 +40,9 @@ else {
 	
 	<title>LOGIN to Nvelopes</title>
 	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 </head>
 <body>
 	<header>
