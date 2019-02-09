bb9f68227a09f85fec54b565f4cd3db8fb76251d
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-09 16:05:44 -0700

    create envelopes section added

--- web/nvelopes/envelopes.php
+++ web/nvelopes/envelopes.php
@@ -9,24 +9,24 @@ if(!isset($_SESSION['userID'])){
 else {
 	//query for envelopes and display them
 	$sql = ' SELECT
-				 envelope.id,
-				 color,
-				 name,
-				 SUM (amount) AS total
-				FROM
-				 public.transaction
-				 INNER JOIN public.envelope
-				 ON transaction.envelopeid = envelope.id
-				 WHERE
-				 userid = :userID
-				GROUP BY
-				 name, envelope.id;';
+	envelope.id,
+	color,
+	name,
+	SUM (amount) AS total
+	FROM
+	public.transaction
+	INNER JOIN public.envelope
+	ON transaction.envelopeid = envelope.id
+	WHERE
+	userid = :userID
+	GROUP BY
+	name, envelope.id;';
 
 	$stmt = $db->prepare($sql);
 	$stmt->bindValue(':userID', $_SESSION['userID']);
 	$stmt->execute();
 	$rowsArray = $stmt->fetchALL(PDO::FETCH_ASSOC);
- }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -37,7 +37,7 @@ else {
 	
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
- 
+
 	<title>Nvelopes</title>
 	<link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
@@ -57,25 +57,35 @@ else {
 	</header>
 	<!-- MAIN SECTION-->
 	<div class="container">
-	  <h2>Nvelopes</h2>
-	  <p>Simple Budgeting Web App</p>
-	  <ul class="nav nav-pills nav-justified">
-	    <li class="active"><a href="#view" data-toggle="tab">View Envelopes</a></li>
-	    <li><a href="#create" data-toggle="tab">Create Envelope</a></li>
-	    <li><a href="#transactions" data-toggle="tab">Transactions</a></li>
-	  </ul>
+		<h2>Nvelopes</h2>
+		<p>Simple Budgeting Web App</p>
+		<ul class="nav nav-pills nav-justified">
+			<li class="active"><a href="#view" data-toggle="tab">View Envelopes</a></li>
+			<li><a href="#create" data-toggle="tab">Create Envelope</a></li>
+			<li><a href="#transactions" data-toggle="tab">Transactions</a></li>
+		</ul>
 
-  <div class="tab-content">
-				<div class="tab-pane fade in active"  id="view">
-					<p>First para Hi there 
+		<div class="tab-content">
+			<div class="tab-pane fade in active"  id="view">
+				<p>First para Hi there </p>
 				</div>
 				<div class="tab-pane fade"  id="create">
-					<p>Second para Hi there 
+					<form id="createEnvelope"action="includes/envelope_inc.php" method="post" class="form-group col-md-6">
+						<center><h2>Create Envelope</h2></center><br>
+						<input type="text" placeholder="Envelope Name" name="name" class="form-control" required><br>
+						<input type="text" placeholder="Description" name="desc" class="form-control" required><br>
+						
+						<input type="number" placeholder="Warning amount ie: 5.00" name="warningAmount" class="form-control" required>
+						<p>If envelope total drops below warning value, envelope will turn red</p><br>
+						<input type="color" placeholder="" name="color" class="form-control" required>
+						<p>Select color for envelope. **</p><br>
+						<input type="submit" name="createEnvelope" class="btn btn-primary btn-block">
+					</form> 
+					</div>
+					<div class="tab-pane fade"  id="transactions">
+						<p>Third para Hi there 
+						</div>
+					</div>
 				</div>
-				<div class="tab-pane fade"  id="transactions">
-					<p>Third para Hi there 
-				</div>
-			</div>
-</div>
 
-<?php include "templates/footer.php"?>
+				<?php include "templates/footer.php"?>
