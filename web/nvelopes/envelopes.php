b581d02672661e7c838a3f58d62ac163fc7b4199
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-09 15:45:12 -0700

    trying new boostrap link

--- web/nvelopes/envelopes.php
+++ web/nvelopes/envelopes.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 include 'includes/dbh_inc.php';
-include "templates/header.php";
+// include "templates/header.php";
 if(!isset($_SESSION['userID'])){
 	header("Location: login.php?login=noAuth");
 	exit();
@@ -28,7 +28,16 @@ else {
 	$rowsArray = $stmt->fetchALL(PDO::FETCH_ASSOC);
  }
 ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+ 
 	<title>Nvelopes</title>
 	<link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
