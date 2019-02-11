d05258c866a93d748f29d385cf4c3b14bb4c5e9d
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-11 13:04:57 -0700

    fix spelling

--- web/nvelopes/transactions.php
+++ web/nvelopes/transactions.php
@@ -99,7 +99,7 @@ else {
 
 	<script>
 		function addTransaction(envelopeID, userID, date, details, amount, type) {
-	$.post('inlcudes/ajax_inc.php',
+	$.post('includes/ajax_inc.php',
 		{"envelopeID": envelopeID, "userID": userID, "date": date, "details":details, "amount": amount, "type": 'add'},
 			function (returnedData) {
 				console.log(returnedData);
