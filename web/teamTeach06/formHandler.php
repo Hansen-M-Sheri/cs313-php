94aa62fe8c4a6dffc8828b9d090cf13083bcd100
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-12 11:57:05 -0700

    finding 500

--- web/teamTeach06/formHandler.php
+++ web/teamTeach06/formHandler.php
@@ -4,31 +4,31 @@ $if(!isset($_POST['Submit'])){
 	exit();
 }
 else {
-	$book = $_POST['book'];
-	$chapter = $_POST['chapter'];
-	$verse = $_POST['verse'];
-	$content = $_POST['content'];
-	$topics = $_POST['topic'];
+// 	$book = $_POST['book'];
+// 	$chapter = $_POST['chapter'];
+// 	$verse = $_POST['verse'];
+// 	$content = $_POST['content'];
+// 	$topics = $_POST['topic'];
 
-	//insert scripture 
-	$stmt = $db->prepare('INSERT INTO Scriptures(book,chapter,verse,content)
-		VALUES(
-		    :book,
-		    :chapter,
-		    :verse,
-		    :content
-		);
-');
-	$stmt->bindValue(':book', $book);
-	$stmt->bindValue(':chapter', $chapter);
-	$stmt->bindValue(':verse', $verse);
-	$stmt->bindValue(':content', $content);
-	$stmt->execute();
-	$itemID = $db->lastInsertID('Scripture_id_seq');
-	//insert into 
-	foreach($topics as $topic){
-		 $db->query("INSERT INTO Scriptures_to_Topic(Scriptures_id,Topic_id)
-                VALUES($itemID,$topic);");
+// 	//insert scripture 
+// 	$stmt = $db->prepare('INSERT INTO Scriptures(book,chapter,verse,content)
+// 		VALUES(
+// 		    :book,
+// 		    :chapter,
+// 		    :verse,
+// 		    :content
+// 		);
+// ');
+// 	$stmt->bindValue(':book', $book);
+// 	$stmt->bindValue(':chapter', $chapter);
+// 	$stmt->bindValue(':verse', $verse);
+// 	$stmt->bindValue(':content', $content);
+// 	$stmt->execute();
+// 	$itemID = $db->lastInsertID('Scripture_id_seq');
+// 	//insert into 
+// 	foreach($topics as $topic){
+// 		 $db->query("INSERT INTO Scriptures_to_Topic(Scriptures_id,Topic_id)
+//                 VALUES($itemID,$topic);");
 	
-	}
+// 	}
 }
