2ad31178b86bed6edbae850d999855b4f9820d52
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-12 12:06:58 -0700

    finding 500

--- web/teamTeach06/formHandler.php
+++ web/teamTeach06/formHandler.php
@@ -25,6 +25,10 @@ else {
 	$stmt->bindValue(':content', $content);
 	$stmt->execute();
 	$itemID = $db->lastInsertID('Scripture_id_seq');
+	if(!$itemID > 1 ){
+		header("Location: team06.php?id=$itemID");
+		exit();
+	}
 	//insert into 
 	foreach($topics as $topic){
 		 $db->query("INSERT INTO Scriptures_to_Topic(Scriptures_id,Topic_id)
