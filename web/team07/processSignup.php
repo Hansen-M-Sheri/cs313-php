6eecb49561fefc3404786d157004637708320d1b
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-16 09:40:38 -0700

    added php to handle signup

--- web/team07/processSignup.php
+++ web/team07/processSignup.php
@@ -1,16 +1,54 @@
 <?php
 include 'dbh.php';
 //Get post values for username & password
+if(isset($_POST['username'])){
+	$user = htmlspecialchars($_POST['username']);
+	$pwd  = htmlspecialchars($_POST['password']);
+	$pwdVerify = htmlspecialchars($_POST['passwordVerify']);
 
-//is username unique? if yes go to password else 
+	//stretch 1 - do password fields match, if not return error
+	if($pwd != $pwdVerify){
+		$newURL = 'signup.php?error=passwordMatch';
+		header("Location: $newURL");
+		die();
+	}
+	else {
+		//stretch 2 - password must be 7 characters & have a number or error
 
-//stretch 1 - do password fields match, if not return error
+		//hash password 
+		$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+		//insert user to db
+		$sql = 'INSERT INTO team.user (username, password) VALUES (:username, :pwd);';
+		$stmt = $db->prepare($sql);
+		$stmt->bindValue(':username', $user);
+		$stmt->bindValue(':pwd', $hashedPwd);
+		$stmt->execute();
+		$userID = $db->lastInsertID('user_id_seq');
+		if($userID < 1) { //failed insert
+			$newURL = 'signup.php?error=dbInputUser';
+			header("Location: $newURL");
+			die();
+		}
+		else {
+			$newURL = 'signin.php';
+			header("Location: $newURL");
+			die();
+		}
+	}
+}
+else {
+	$newURL = 'signup.php?error=noUsername';
+	header("Location: $newURL");
+	die();
+}
 
-//stretch 2 - password must be 7 characters & have a number or error
 
-//hash the password and store it
 
-//insert user to db
+
+
+
+
+
 
 //redirect to sign-in page
 
--- web/team07/signin.php
+++ web/team07/signin.php
@@ -8,6 +8,7 @@
 	<form action="processSignin.php" method="post">
 		<input type="text" name="username" placeholder="Username">
 		<input type="password" name="password" placeholder="Password">
+		<input type="submit" value="Sign In">
 	</form>
 </body>
 </html>
