14657bb55ea7ffaa0860a6fcf8f3ea24513f4b25
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-16 10:05:11 -0700

    adding signin processing

--- web/team07/processSignin.php
+++ web/team07/processSignin.php
@@ -1,9 +1,31 @@
 <?php
 session_start();
+include 'dbh.php';
 //get the POST from signin
+if(isset($_POST['username'])){
+	$user = htmlspecialchars($_POST['username']);
+	$pwd  = htmlspecialchars($_POST['password']);
 
-//Get password from username in db
+	//Get password from username in db
+	$sql = 'SELECT password FROM team.user WHERE username=:username;';
+	$stmt = $db->prepare($sql);
+	$stmt->bindValue(':username', $user);
+	$stmt->execute();
+	$rowsArray = $stmt->fetchALL(PDO::FETCH_ASSOC);
+	if($rowsArray < 1){ 
+		$newURL = 'signin.php?error=usernameDB';
+		header("Location: $newURL");
+		die();
+	}
+	else {
+		//compare user entered pwd against hashed pwd
+		echo var_dump($rowsArray);
+		$pwd = $rowsArray[0];
+		echo $pwd;
+	}
+}
 
-//user password_verify()
+
+
 
 //if match save user to session
