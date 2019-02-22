150af12f65d5d21cf0f9e9e3fdbae0ddbcc9f606
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-22 10:52:34 -0700

    added jumbotron, adjusted buttons and removed echo on createEnvelope

--- web/nvelopes2/createEnvelope.php
+++ web/nvelopes2/createEnvelope.php
@@ -8,8 +8,6 @@ if(!isset($_SESSION['userID'])){
 }
 else {
 	$envelopeName = " ";
-	echo "GET: ";
-	echo $_GET;
 	//query for envelopes and display them
 	// echo "test";
 	$sql = ' SELECT
@@ -30,12 +28,6 @@ else {
 	$stmt->bindValue(':userID', $_SESSION['userID']);
 	$stmt->execute();
 	$rowsArray = $stmt->fetchALL(PDO::FETCH_ASSOC);
-	
-	foreach ($rowsArray as $row) {
-		echo $row['name'];
-		echo $row['total'];
-		echo '<br>';
-	}
 }
 ?>
 	
@@ -53,30 +45,25 @@ else {
 			</ul>
 		</nav>
 	</header>
-
-	<div class="row col-md-8">
-		<ul class="nav nav-tab">
-			<li><a data-toggle="tab" href="#create" class="btn btn-dark btn-tab">Create Envelope</a></li>
+	<!-- Jumbotron -->
+    <div class="jumbotron bg-info" id="banner">
+      <h1>Create </h1>
+      <h3>Create a new envelope and start choosing where your money will go</h3>
+  	</div>
+  	<div class="container">
+		<form id="createEnvelope"action="includes/envelope_inc.php" method="post" class="form-group col-md-6">
+			<center><h2>Create Envelope</h2></center><br>
+			<input type="text" placeholder="Envelope Name" name="name" class="form-control" required><br>
+			<input type="text" placeholder="Description" name="desc" class="form-control" required><br>
+				
+			<input type="number" placeholder="Warning amount ie: 5.00" name="warningAmount" class="form-control" required>
+			<p>If envelope total drops below warning value, envelope will turn red</p><br>
+			<input type="color" placeholder="" name="color" class="form-control" required>
+			<p>Select color for envelope. **</p><br>
+			<input type="submit" name="createEnvelope" class="btn btn-primary btn-block" value="Create Envelope">
+		</form>	
 			
-		</ul>
-		<div class="tab-content" style="">
-			<div id="create" class="tab-pane active">
-					<form id="createEnvelope"action="includes/envelope_inc.php" method="post" class="form-group col-md-6">
-						<center><h2>Create Envelope</h2></center><br>
-						<input type="text" placeholder="Envelope Name" name="name" class="form-control" required><br>
-						<input type="text" placeholder="Description" name="desc" class="form-control" required><br>
-						
-						<input type="number" placeholder="Warning amount ie: 5.00" name="warningAmount" class="form-control" required>
-						<p>If envelope total drops below warning value, envelope will turn red</p><br>
-						<input type="color" placeholder="" name="color" class="form-control" required>
-						<p>Select color for envelope. **</p><br>
-						<input type="submit" name="createEnvelope" class="btn btn-primary btn-block">
-					</form>	
-			</div>
-      </div><!--column-->
-      
-      
-  </div><!--row-->
+  </div>
 					
 
 <?php include "templates/footer.php"; ?>
--- web/nvelopes2/signup.php
+++ web/nvelopes2/signup.php
@@ -13,14 +13,20 @@
 			</ul>
 		</nav>
 	</header>
-	<!-- Nav bar to HOME = signup, Login button-->
-<form class="form-group col-md-6" action="includes/login_inc.php" method="POST">
-	<center><h2>Signup</h2></center><br>
-	<input type="text" placeholder="First Name" name="fName" class="form-control" required><br>
-	<input type="text" placeholder="Last Name" name="lName" class="form-control" required> <br>
-	<input type="email" placeholder="Email" name="email" class="form-control" required><br>
-	<input type="password" placeholder="Password" name="pwd" class="form-control" required><br>
-	<input type="submit" name="submitSignup" class="btn btn-success btn-block">
-</form>
+<!-- Jumbotron -->
+    <div class="jumbotron bg-info" id="banner">
+      <h1>Sign up</h1>
+      <h3>Sign up and start saving money now!</h3>
+  	</div>
+  	<div class="container">
+		<form class="form-group col-md-6" action="includes/login_inc.php" method="POST">
+			<center><h2>Signup</h2></center><br>
+			<input type="text" placeholder="First Name" name="fName" class="form-control" required><br>
+			<input type="text" placeholder="Last Name" name="lName" class="form-control" required> <br>
+			<input type="email" placeholder="Email" name="email" class="form-control" required><br>
+			<input type="password" placeholder="Password" name="pwd" class="form-control" required><br>
+			<input type="submit" name="submitSignup" class="btn btn-success btn-block">
+		</form>
+	</div>
 <?php include "templates/footer.php"; ?>
 
