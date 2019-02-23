708718775d1995a4bc6aa01900af8461be4c0e53
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-23 16:59:09 -0700

    finding error with adjustTransaction

--- web/nvelopes2/includes/adjustTransaction_inc.php
+++ web/nvelopes2/includes/adjustTransaction_inc.php
@@ -1,11 +1,17 @@
 <?php
 if(!isset($_GET['transactionID'])){
-	header("Location: ../transactions.php");
-	die();
+	echo "transactionID not set";
+
+	// header("Location: ../transactions.php");
+	// die();
 }
 else {
+	echo var_dump($_GET);
 	//remove item from db
 	$sql = "DELETE FROM public.transaction WHERE id=:id";
 	$stmt = $db->prepare($sql);
 	$stmt->bindValue(':id', $_GET['transactionID']);
+	$stmt->execute();
+	//refresh transactions.php
+	// header("Location: ../transactions.php?envID=$_GET['envelopeID']");
 }
