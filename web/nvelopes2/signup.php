101317b1e949dcaef9c4189df01c0419d0214ab2
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-23 15:45:22 -0700

    added signup errors

--- web/nvelopes2/signup.php
+++ web/nvelopes2/signup.php
@@ -1,9 +1,26 @@
 <!-- SIGN UP page - Home page is here -->
-<?php include "templates/header.php";?>
-<!-- Login form - process with login_inc.php-->
+<?php 
+include "templates/header.php";
+// Handle any errors
+if(isset($_GET['signup'])){
+	$error = $_GET['signup'];
+	$errMsg = "";
+	if($error == 'empty'){
+		$errMsg = "All fields must be entered to signup";
+	}
+	else if ($error == 'invalidName'){
+		$errMsg = "Please enter a valid name using letters a - z";
+	}
+	else if ($error == 'email'){
+		$errMsg = "Please enter a valid email";
+	}
+	else if ($error == 'userTaken'){
+		$errMsg = "That email already exists, please login";
+	}
+}
+?>
 	
 	<title>Sign up for Nvelopes</title>
-	<link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
 <body>
 	<header>
@@ -18,6 +35,10 @@
       <h1>Sign up</h1>
       <h3>Sign up and start saving money now!</h3>
   	</div>
+  	<?php
+  		if(!empty($error)){
+  			echo "<div class='error'>".$errMsg."</div>";
+  		}?>
   	<div class="container">
 		<form class="form-group col-md-6 col-md-offset-3" action="includes/signup_inc.php" method="POST">
 			<center><h2>Signup</h2></center><br>
