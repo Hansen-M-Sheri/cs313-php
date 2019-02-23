29201010e5dc07b5ba287b962d2c768fd5ddd105
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-22 19:11:11 -0700

    edit envelope updating correclty

--- web/nvelopes2/includes/editEnvelope_inc.php
+++ web/nvelopes2/includes/editEnvelope_inc.php
@@ -13,17 +13,8 @@ if(isset($_POST['editEnvelope'])){
 	$color		= htmlspecialchars($_POST['color']);
 
 	if(empty($id) || empty($description) || empty($warningAmt) || empty($color)){
-		echo "empty variable";
-		echo "id: ";
-		echo $id;
-		echo "description: ";
-		echo $description;
-		echo "warningAmt: ";
-		echo $warningAmt;
-		echo "color: ";
-		echo $color;
-		// header("Location: ../editEnvelope.php?envelope=empty");
-		// exit();
+		header("Location: ../editEnvelope.php?envelope=empty");
+		exit();
 	}
 	else {
 		$sql = 'UPDATE public.envelope SET description=:description, warningamount=:warningAmt, color=:color WHERE id=:id;';
@@ -33,20 +24,14 @@ if(isset($_POST['editEnvelope'])){
 		$stmt->bindValue(':color', $color);
 		$stmt->bindValue(':id', $id);
 		$stmt->execute();
-		// $newItemID =  $db->lastInsertID('envelope_id_seq');
-		// if($newItemID < 1){
-		// 	header("Location: ../editEnvelope.php?create=error");
-		// } else {
-			//redirect to envelope page
-		echo "success";
-			// header("Location: ../setup.php?update=success");
-		// }
+		//redirect to envelope page
+		header("Location: ../setup.php?update=success");
+
 	}			
 }
 else {
-	echo "editEnvelope not set";
-	// header("Location: ../setup.php?update=accidental");
-	// die();
+	header("Location: ../setup.php?update=accidental");
+	die();
 }
 
 
