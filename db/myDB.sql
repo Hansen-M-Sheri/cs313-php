1dd5fd97d7480ebb42c14bd6fdccc6ea9e5136ae
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-02-23 18:15:38 -0700

    added items to db file

--- db/myDB.sql
+++ db/myDB.sql
@@ -1,4 +1,4 @@
-//DB for Nvelopes
+-- DB for Nvelopes
 
 CREATE TABLE public.user
 (
@@ -30,3 +30,111 @@ CREATE TABLE public.transaction
 	details TEXT,
 	amount DECIMAL(15,2)
 );
+
+
+//VERIFY EMAIL EXISTS IN DB
+$stmt = $db->prepare('SELECT email FROM public.user WHERE email=:email');
+				$stmt->bindValue(':email', $email);
+				$stmt->execute();
+				$rowsArray = $stmt->fetchALL(PDO::FETCH_ASSOC);
+				
+<--insert user into database-->
+$sql = 'INSERT INTO public.user (fName, lName, email, password, secretquestion1answer, secretquestion2answer) VALUES (:fName, :lName, :email, :hashedPwd, :secret1, :secret2)';
+					$stmt = $db->prepare($sql);
+
+					//pass values to statement
+					$stmt->bindValue(':fName', $fName);
+					$stmt->bindValue(':lName', $lName);
+					$stmt->bindValue(':email', $email);
+					$stmt->bindValue(':hashedPwd', $hashedPwd);
+					$stmt->bindValue(':secret1', $secret1);
+					$stmt->bindValue(':secret2', $secret2);
+					
+					$stmt->execute();
+
+					$id =  $db->lastInsertID('user_id_seq');
+					
+<--insert user into database-->
+$sql = 'INSERT INTO public.transaction (envelopeid, userid, date, details, amount) VALUES (:envelopeID, :userID, :date, :details, :amount)';
+					$stmt = $db->prepare($sql);
+
+					//pass values to statement
+					$stmt->bindValue(':fName', 1);
+					$stmt->bindValue(':userID', 1);
+					$stmt->bindValue(':date', '1/1/19');
+					$stmt->bindValue(':details', 'Maverick');
+					$stmt->bindValue(':amount', '50.00');
+					
+					
+					$stmt->execute();
+
+					$id =  $db->lastInsertID('transaction_id_seq');
+					
+					
+					
+<!-- if sessionId is set, get all envelopes from userID (stored in session)-->
+SELECT
+ envelopeid,
+ SUM (amount) AS total
+FROM
+ public.transaction
+ WHERE
+ userid = 1
+GROUP BY
+ envelopeid;
+
+
+--  SUM the envelope total 
+ SELECT envelopeid, envelope.name, SUM (amount) AS total FROM public.transaction INNER JOIN public.envelope 
+ ON transaction.envelopeid = envelope.id WHERE userid=1 GROUP BY transaction.envelopeid;
+ 
+ SELECT envelopeid, name, SUM (amount) AS total FROM public.transaction CROSS JOIN public.envelope WHERE userid=1 GROUP BY envelopeid;
+ 
+ SELECT
+ name,
+ SUM (amount) AS total
+FROM
+ public.transaction
+ INNER JOIN public.envelope
+ ON transaction.envelopeid = envelope.id
+ WHERE
+ userid = 1
+GROUP BY
+ name;
+-- GET envelope for 1 user and 1 envelope (transactions table)
+SELECT
+	SUM (amount) AS total
+	FROM
+		public.transaction
+	WHERE
+		envelopeid = :envelopeID
+	AND
+		 userid = :userID;
+				  
+ SELECT
+ envelope.id,
+ color,
+ name,
+ SUM (amount) AS total
+FROM
+ public.transaction
+ INNER JOIN public.envelope
+ ON transaction.envelopeid = envelope.id
+ WHERE
+ userid = 1
+GROUP BY
+ name, envelope.id;
+ 
+ SELECT 
+  name, 
+  date,
+  details,
+  amount
+ FROM 
+ public.transaction
+ 
+ SELECT id FROM envelope WHERE name='Gas';
+ INSERT INTO transactions(envelopeid, userid, date, details, amount) VALUES (:envelopeID, :userID, :date, :details, :amount);
+ 
+--  update/edit envelope settings
+UPDATE public.envelope SET description='groceries' WHERE id=2;
